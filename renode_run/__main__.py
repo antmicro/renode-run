767edad051fd79906f3dbb750f85d8c14401d64a
Mateusz Karlic <mkarlic@antmicro.com>
2023-07-07 09:47:31 +0200

    Add scroll offset

--- renode_run/__main__.py
+++ renode_run/__main__.py
@@ -268,7 +268,7 @@ def download_command(args):
     download_renode(target_dir_path, renode_run_config_path, args.version, args.direct)
 
 def get_fuzzy_or_none(alternatives: 'list[str]', query: 'str|None' = False, do_prints: bool = True) -> 'str|None':
-    FZF_STYLE = "--height=80% --layout=reverse --info=inline --border --margin=1 --padding=1"
+    FZF_STYLE = "--height=80% --layout=reverse --info=inline --border --margin=1 --padding=1 --scroll-off=3"
     FZF_DEFAULTS = "-i --cycle"
 
     opts = ' '.join([FZF_STYLE, FZF_DEFAULTS])
