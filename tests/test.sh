5f8aa6bd9376358d110112ef5446b38654cec7d4
Michał Konstańczak-Szprejda <mszprejda@antmicro.com>
2026-04-24 13:20:28 +0200

    [#94796] Set Renode params in variable

--- tests/test.sh
+++ tests/test.sh
@@ -14,6 +14,16 @@ ELF_LINK=https://dl.antmicro.com/projects/renode/renode-nrf52840-zephyr_shell_mo
 
 ROBOT_TEST="tests/example.robot"
 
+PARAMS=""
+case "$OSTYPE" in
+  linux*)
+    PARAMS="--console --disable-xwt --plain"
+    ;;
+  *)
+    PARAMS="--disable-xwt --plain"
+    ;;
+esac
+
 assert_path_exists()
 {
   path="$1"
@@ -54,7 +64,7 @@ run_test()
 #This behaviour occurs for every command except download.
 test_default_behaviour()
 {
-  renode-run -- --console --disable-xwt --plain -e "q"
+  renode-run -- $PARAMS -e "q"
   assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/dotnet-portable/renode-*"
   delete_test_files
 }
@@ -63,14 +73,22 @@ test_default_behaviour()
 #so there is no need to test this option for all of them.
 test_default_behaviour_with_custom_artifacts_path()
 {
-  renode-run -a $TEST_ARTIFACTS_PATH -- --console --disable-xwt --plain -e "q"
+  renode-run -a $TEST_ARTIFACTS_PATH -- $PARAMS -e "q"
   assert_path_exists "$TEST_ARTIFACTS_PATH/renode-run.download/dotnet-portable/renode-*"
+  delete_test_files
+}
+
+test_default_behaviour_with_renode_dotnet_portable()
+{
+  renode-run download --renode-variant dotnet-portable
+  renode-run --renode-variant dotnet-portable -- $PARAMS -e "q"
+  assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/dotnet-portable/renode-*"
   delete_test_files
 }
 
 test_using_exec_command_explicitly()
 {
-  renode-run exec -- --console --disable-xwt --plain -e "q"
+  renode-run exec -- $PARAMS -e "q"
   assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/dotnet-portable/renode-*"
   delete_test_files
 }
@@ -79,7 +97,7 @@ test_downloading_to_default_location()
 {
   renode-run download
   assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/dotnet-portable/renode-*"
-  renode-run -- --console --disable-xwt --plain -e "q"
+  renode-run -- $PARAMS -e "q"
   delete_test_files
 }
 
@@ -88,7 +106,7 @@ test_downloadinng_to_selected_location()
   renode-run download --path $TEST_DOWNLOAD_PATH
   assert_path_exists "$TEST_DOWNLOAD_PATH/dotnet-portable/renode-*"
   #Renode installation is one directory above $TEST_DOWNLOAD_PATH so it has to be passed as artifacts path.
-  renode-run -a $TEST_DOWNLOAD_PATH/.. -- --console --disable-xwt --plain -e "q"
+  renode-run -a $TEST_DOWNLOAD_PATH/.. -- $PARAMS -e "q"
   delete_test_files
 }
 
@@ -110,9 +128,17 @@ test_downloading_selected_renode_version()
 test_downloading_without_creating_directories_for_versions()
 {
   renode-run download -d
-  assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/renode"
-  assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/renode-test"
-  renode-run -- --console --disable-xwt --plain -e "q"
+  case "$OSTYPE" in
+    linux*)
+      assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/renode"
+      assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/renode-test"
+      ;;
+    *)
+      assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/renode.exe"
+      assert_path_exists "$DEFAULT_ARTIFACTS_PATH/renode-run.download/renode-test.bat"
+      ;;
+  esac
+  renode-run -- $PARAMS -e "q"
   delete_test_files
 }
 
@@ -137,13 +163,13 @@ test_using_custom_venv_directory()
 test_running_dashboard_demo()
 {
   #This is a simplified test which doesn't verify if Renode actually executes the demo.
-  renode-run demo --board $BOARD $SAMPLE -- --console --disable-xwt --plain -e "q"
+  renode-run demo --board $BOARD $SAMPLE -- $PARAMS -e "q"
   delete_test_files
 }
 
 test_saving_repl_and_dts()
 {
-  renode-run demo -g --board $BOARD $SAMPLE -- --console --disable-xwt --plain -e "q"
+  renode-run demo -g --board $BOARD $SAMPLE -- $PARAMS -e "q"
   assert_path_exists "$(pwd)/$BOARD.repl"
   assert_path_exists "$(pwd)/$BOARD.dts"
   delete_test_files
@@ -153,7 +179,7 @@ test_running_local_elf()
 {
   curl -o $ELF_PATH $ELF_LINK
   #This is a simplified test which doesn't verify if Renode actually executes the demo.
-  renode-run demo --board $BOARD $ELF_PATH -- --console --disable-xwt --plain -e "q"
+  renode-run demo --board $BOARD $ELF_PATH -- $PARAMS -e "q"
   delete_test_files
 }
 
